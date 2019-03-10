b12c7f19d81f6ddf16665b587df6c1535083c436
DC <just-one@hotmail.co.jp>
2019-03-10 11:34:39 +0900

    いろんな関数を定義した

---
+++ sample05.py
@@ -0,0 +1,21 @@
+# 2分課題:　2つの整数の和を返す関数　add を作ってね
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+y = add(a=3, b=4)
+
+print(y)  # ここで7が出力されればOK!!
+
+
+# 自流
+def add(a, b: int) -> int:
+    return a + b
+
+
+y = add(a=3, b=4)
+
+print(y)
+#
+# print(y)
